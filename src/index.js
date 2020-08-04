d7e9a97d82e500b5aa9df7d46a9ad56b20ca56b3
Yasir <Yasir.A.Kulane@gmail.com>
2020-08-04 16:27:14 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,32 +1,52 @@
-
 let todos = [
-    {
-        completed: false,
-        description: "Take Jim to the hair salon"
-    },
-    {
-        completed: true,
-        description: "Drop off wedding invitation at mailbox"
-    },
-    {
-        completed: false,
-        description: "Pick up the cake"
-    },
-    {
-        completed: false,
-        description: "Call the caterers"
-    }
+  {
+    completed: false,
+    description: "Take Jim to the hair salon",
+  },
+  {
+    completed: true,
+    description: "Drop off wedding invitation at mailbox",
+  },
+  {
+    completed: false,
+    description: "Pick up the cake",
+  },
+  {
+    completed: false,
+    description: "Call the caterers",
+  },
 ];
 
+// my code here
+
 function renderTodoApp() {
-    let app = document.querySelector("#app")
-    let h1 = document.createElement("h1")
-    // your code here
+  let app = document.querySelector("#app");
+  let h1 = document.createElement("h1");
+  //
 
-    h1.innerText = "Todo List"
+  h1.innerText = "Todo List";
 
-    app.appendChild(h1)
-    // and maybe some here
+  app.appendChild(h1);
+  //
+
+  const list = document.createElement("ul");
+  document.body.append(list);
+
+  for (let i = 0; i < todos.length; i++) {
+    let myList = todos[i];
+    let listMarkup = `
+        
+        <li class="list">  
+        <input type="checkbox" ${myList.completed ? "checked" : "unchecked"}>
+                <label> ${myList.completed} ${myList.description} </label>
+
+            </input>
+
+        </li>
+        `;
+
+    list.innerHTML += listMarkup;
+  }
 }
 
-renderTodoApp()
+renderTodoApp();
