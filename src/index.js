ccf4f6fe41239a8b2866a9186a5b7d372f4bddce
Yasir <Yasir.A.Kulane@gmail.com>
2020-08-06 02:19:10 -0500

    done, man I'm so sleepy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// The contents of this list manage the state of each object in the DOM
 let todos = [
   {
     completed: false,
@@ -17,36 +18,49 @@ let todos = [
   },
 ];
 
-// my code here
+// This function renderd the items in the UL at the beginning
+// and also each time the addToList() function is called
+function renderTodoList() {
+  let listContainer = document.getElementById("listView"); // get the ul container
 
-function renderTodoApp() {
-  let app = document.querySelector("#app");
-  let h1 = document.createElement("h1");
-  //
+  listContainer.innerHTML = ""; // clear it
 
-  h1.innerText = "Todo List";
-
-  app.appendChild(h1);
-  //
-
-  const list = document.createElement("ul");
-  document.body.append(list);
-
+  // Add each item back to the list with its new / current state from 'todos'
   for (let i = 0; i < todos.length; i++) {
     let myList = todos[i];
+
     let listMarkup = `
-        
-        <li class="list">  
-        <input type="checkbox" ${myList.completed ? "checked" : "unchecked"}>
-                <label> ${myList.completed} ${myList.description} </label>
+          <li class="list">  
+              <input id="${i}" onclick="toggleItem(this)" type="checkbox" ${
+      myList.completed ? "checked" : "unchecked"
+    }>
+                  <label> ${myList.completed} ${myList.description} </label>
+              </input>
+          </li>`;
 
-            </input>
-
-        </li>
-        `;
-
-    list.innerHTML += listMarkup;
+    listContainer.innerHTML += listMarkup; // this adds each item to the ul
   }
 }
 
-renderTodoApp();
+// This function adds a new list item to 'todos' and re-renders the whole list
+function addToList() {
+  var content = document.getElementById("input").value; // get the value of the input box (its contents)
+
+  var newitem = { completed: false, description: content }; // create new item data for 'todos'
+
+  todos.push(newitem); // add it to the list
+
+  renderTodoList(); // re-render the whole list, including the new item
+}
+
+// this makes sure when the item is toggles, its updated in the 'todos' list as well
+function toggleItem(targetItem) {
+  todos[targetItem.id].completed = targetItem.checked;
+  renderTodoList();
+}
+
+// this is called once the javascrip file in initially read, to render the initial data
+// In the html youll see that the scrip tag is at the bottom instead of in the head tag
+// This is so that the JS will be run after the HTML page DOM is rendered, so the items
+// arent returned null when JS tries to pull them and edit them
+renderTodoList();
